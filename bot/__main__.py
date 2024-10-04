efc198f0eb10bc64ccbd0d7df39c3eac49609196
tftiwnbe <wnbe@hmphin.space>
2024-10-04 21:43:14 +0300

    Use class for bot creating

--- bot/__main__.py
+++ bot/__main__.py
@@ -10,38 +10,42 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = getenv("BOT_TOKEN")
 
+class AiogramBot:
+    def __init__(self, token: str):
+        self.bot = Bot(
+            token=token, default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+        )
+        self.dp = Dispatcher()
 
-dp = Dispatcher()
+        # Handler registration
+        self.dp.message.register(self.command_start_handler, CommandStart())
+        self.dp.message.register(self.echo_handler)
 
+    async def command_start_handler(self, message: Message) -> None:
+        """
+        This handler receives messages with `/start` command
+        """
+        await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    await message.answer(f"Hell, {html.bold(message.from_user.full_name)}!")
+    async def echo_handler(self, message: Message) -> None:
+        """
+        Handler will forward received message back to the sender
+        """
+        try:
+            await message.send_copy(chat_id=message.chat.id)
+        except TypeError:
+            await message.answer("Nice try!")
 
-
-@dp.message()
-async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.answer("Nice try!")
+    async def start(self):
+        await self.dp.start_polling(self.bot)
 
 
 async def main() -> None:
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
-    await dp.start_polling(bot)
+    load_dotenv()
+    TOKEN = getenv("BOT_TOKEN")
+    bot = AiogramBot(token=TOKEN)
+    await bot.start()
 
 
 if __name__ == "__main__":
