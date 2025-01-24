1b2092b853b1d27d6e45701436e1d4b414abd886
wnbe <wnbe@hmphin.space>
2025-01-24 21:11:55 +0300

    feat: add filter for active users

--- bot/filters.py
+++ bot/filters.py
@@ -1,6 +1,10 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 from config import settings
+from database import get_db_session
+from database.crud.user import UserNotFoundError, get_user
+from database.schemas.user import UserSearch
+from loguru import logger
 
 
 class IsSuperuser(BaseFilter):
@@ -12,3 +16,23 @@ class IsSuperuser(BaseFilter):
                 return False
         else:
             return False
+
+
+class IsActiveUser(BaseFilter):
+    async def __call__(self, message: Message) -> bool:
+        if message.from_user is None:
+            return False
+
+        user_data = UserSearch(tg_user_id=message.from_user.id)
+
+        try:
+            async for session in get_db_session():
+                user = await get_user(session, user_data)
+                return bool(user and user.is_active)
+        except UserNotFoundError:
+            return False
+        except Exception as e:
+            logger.error(f"Unexpected error: {e}")
+            return False
+
+        return False
